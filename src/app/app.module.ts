dd47327f5117e1f038df0d8cfe3b7b06ff32839c
Riezzie_3 <richardkerkvliet@hotmail.com>
2020-06-03 16:57:45 +0200

    Setup forum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForumComponent } from './forum/forum.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { ThreadListItemComponent } from './thread-list-item/thread-list-item.component';
+import { ThreadComponent } from './thread/thread.component';
+import { ThreadPostListComponent } from './thread-post-list/thread-post-list.component';
+import { ThreadPostListItemComponent } from './thread-post-list-item/thread-post-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForumComponent,
+    ThreadListComponent,
+    ThreadListItemComponent,
+    ThreadComponent,
+    ThreadPostListComponent,
+    ThreadPostListItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/thread-list-item/thread-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-list-item',
+  templateUrl: './thread-list-item.component.html',
+  styleUrls: ['./thread-list-item.component.css']
+})
+export class ThreadListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
